fdbc39eb21e0e205b13054fd90c2e89e4ff064de
Rômulo Queiroz <rfcontatosvia@gmail.com>
2023-01-05 19:50:11 -0300

    feat: paragrafo dinamico

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 import Seunome from "./components/componenteUm";
 import HelloWorld from "./components/HelloWorld";
+import CriaParagraf from "./components/componenteDois";
 function App() {
   const name = "Rômulo";
   return (
     <div className="App">
       <h1>Hello World</h1>
       <p>Meu nome é: {name}</p>
-
       <Seunome />
+      <CriaParagraf />
     </div>
   );
 }
---
+++ src/components/componenteDois.jsx
@@ -0,0 +1,10 @@
+function CriaParagraf() {
+  let paragrafo = "Este é um parágrafo foi criado por uma função";
+  return (
+    <div>
+      <p>{paragrafo}</p>
+    </div>
+  );
+}
+
+export default CriaParagraf;
