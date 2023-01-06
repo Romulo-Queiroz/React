3fdf83f98a9b8ed504292f4b30bc615aa8e01592
Rômulo Queiroz <rfcontatosvia@gmail.com>
2023-01-06 12:45:34 -0300

    build: css react

--- src/components/Pessoa.jsx
+++ src/components/Pessoa.jsx
@@ -1,10 +1,11 @@
+import styles from "./Pessoa.module.css";
 function Pessoa({ nome, idade, profissao, foto }) {
   return (
     <div>
-      <img src={foto} alt={nome} />
+      <img src={foto} alt={nome} className={styles.sizers} />
       <h2>Nome:{nome}</h2>
       <p>Idade: {idade}</p>
-      <p>Profissão:{profissao}</p>
+      <p className={styles.profissao}>Profissão:{profissao}</p>
     </div>
   );
 }
