0ef7e93c1a2b1759834b29f26179feaf85c4155c
Rômulo Queiroz <rfcontatosvia@gmail.com>
2023-01-06 15:18:45 -0300

    build: evento onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import "./App.css";
-import Seunome from "./components/componenteUm";
-import CriaParagraf from "./components/componenteDois";
-import SayMyName from "./components/SayMyName";
-import Pessoa from "./components/Pessoa";
+import OnClique from "./components/OnClick";
 
 function App() {
   const name = "Rômulo";
   return (
     <div className="App">
-      <p>Meu nome é: {name}</p>
-      <Seunome />
-      <CriaParagraf />
-      <SayMyName name="Romulo" />
-      <SayMyName name="Outro nome" />
-
-      <Pessoa
-        nome="Romulo"
-        idade="25"
-        profissao="Desenvolvedor"
-        foto="https://avatars.githubusercontent.com/u/88904173?v=4"
-      />
+      <OnClique />
     </div>
   );
 }
---
+++ src/components/OnClick.jsx
@@ -0,0 +1,15 @@
+function OnClique() {
+  return (
+    <div>
+      <button
+        onClick={function () {
+          alert("Clicou");
+        }}
+      >
+        Clique aqui
+      </button>
+    </div>
+  );
+}
+
+export default OnClique;
